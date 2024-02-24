e908a37965a83754be25289f557a459cb1980807
Kapil Rai <116628083+Imkapil07@users.noreply.github.com>
2024-02-24 13:51:15 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Fibonacci Sum/fibonacci-sum.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+// } Driver Code Ends
+//User function Template for Java
+class Solution{
+    static long fibSum(long N){
+        if(N==0)
+        {
+            return 0;
+        }
+        if(N==1)
+        {
+            return 1;
+        }
+        long c=0;
+        long a=0;
+        long b=1;
+        long sum=0;
+        for(long i=2;i<=N;i++)
+        {
+           c=a%1000000007+b%1000000007;
+           sum+=c;
+           a=b;
+           b=c;
+        }
+        
+        return (sum+1)%1000000007;
+    }
+}
+
+//{ Driver Code Starts.
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            long N = Long.parseLong(read.readLine());
+           
+            Solution ob = new Solution();
+            System.out.println(ob.fibSum(N));
+        }
+    }
+}
+// } Driver Code Ends
